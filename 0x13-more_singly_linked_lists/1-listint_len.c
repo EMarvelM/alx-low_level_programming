a019cee14c7df14068b29630576be1f172a1ce6b
EMarvelM <marvelousegbe19@gmail.com>
2023-07-28 12:01:02 +0100

    Contains code to count the number of elements in a linked list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,11 @@
 #include "lists.h"
 
-
 /**
- *listint_len - Returns the number of elements
- *in a linked listint_t list
- *@h: A pointer to the head of the list
+ * listint_len - Returns the number of elements in a linked listint_t list
+ * @h: A pointer to the head of the list
  *
- *
- *Return: the number of elements in the listint_t list
+ * Return: the number of elements in the listint_t list
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
@@ -21,4 +17,3 @@ size_t listint_len(const listint_t *h)
 	}
 	return (nodes);
 }
-
