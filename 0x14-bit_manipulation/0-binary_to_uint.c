72bad328dcbe669a383ced3b815b700b921ca8f2
emarvelm <marvelousegbe19@gmail.com>
2023-08-31 13:41:36 +0000

    Handle error case when b is a NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+
 	unsigned int result = 0;
 	int power = 0;
 	int track = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
 
 	while (b[track] != '\0')
 	{
