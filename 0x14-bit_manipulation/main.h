5c84e3c8e94b9a9601504dd4ee4e7b9ac117dee6
emarvelm <marvelousegbe19@gmail.com>
2023-08-31 12:59:50 +0000

    header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h> /* I will be testing */
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+unsigned long int modu2(unsigned long int *digit);
+
+#endif /* MAIN_H */
