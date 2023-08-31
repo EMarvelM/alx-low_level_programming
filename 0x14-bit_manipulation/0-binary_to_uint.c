bca1bb6b2ebdbcf8e19ac5ff85b26b0363fe7542
emarvelm <marvelousegbe19@gmail.com>
2023-08-31 21:16:47 +0000

    converts a binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,29 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to positive number.
- * @b: parameter for pointer to strings of 0s & 1s.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
  *
- * Return: the coverted number if a b is null or 0 when
- * there is neither 1 or 0 in the string
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
+	unsigned int num;
 
-	unsigned int result = 0;
-	int power = 0;
-	int track = 0;
-
-	if (b == NULL)
-	{
+	num = 0;
+	if (!b)
 		return (0);
-	}
-
-	while (b[track] != '\0')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		track++;
-	}
-	track--; /* returned the tracker from the null char */
-
-	while (track >= 0)
-	{
-		if (b[track] == '1')
-		{
-			result = result + (1 << power);
-			power++;
-		}
-		else if (b[track] == '0')
-		{
-			power++;
-		}
-		else
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-		track--;
-
 	}
-	return (result);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
