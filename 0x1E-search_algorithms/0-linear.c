4072b9ff76ba9f2f8c38c58c7ff43d2653c803e4
emarvelm <marvelousegbe19@gmail.com>
2024-04-09 12:42:11 +0100

    treat all warning as error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
-	for (; i < size; i++)
+	for (; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
