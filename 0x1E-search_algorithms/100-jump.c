693af0b11a5e47b41ae0c960ea517500e7618c05
emarvelm <marvelousegbe19@gmail.com>
2024-04-11 10:10:10 +0100

    searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,47 +1,37 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - search an array for a value by segmenting into blocks 
- * of size of square root of the size of the array
- * @array: the array to search
- * @size: the size of te array
- * @value: the value to search from
+ * jump_search - function that searches for a value
+ * in a sorted array of integers
  *
- * Return: -1 if the value is not found and the index if the value was found
+ * @array: a pointer to the first element of the array to search in
+ * @size:  the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located
  */
 
 int jump_search(int *array, size_t size, int value)
 {
-	int low = 0, high, root, i, j;
+	size_t i, step, jump;
 
-	root = sqrt(size);
-	high = (int)root;
-
-
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	/* 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-	*/
-	for(i = low; i < (int)size; i++)
+	step = sqrt(size);
+
+	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
-		if (value == array[low])
-			return (low);
-		else if (value >= array[i] && value <= array[high])
-		{
-			for (j = low; j <= high; j++)
-			{
-				if (value == array[j])
-					return (j);
-			}
-		}
-		else
-		{
-			low = high + 1;
-			high += (int)root;
-		}
-	printf("Verbose\tlow --> %d\n\t\thigh --> %d\n", low, high);
+		printf("Value checked array [%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
 	}
-	return (-1);
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	jump = jump > size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
 }
